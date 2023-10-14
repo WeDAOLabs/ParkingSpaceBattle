254f4cee51be35e8849f3780c2aab571787c06a6
0xSkipper <0xSkipper@gmail.com>
2023-10-14 10:49:31 +0800

    vue3+ts 脚手架

---
+++ client/website/src/components/Demo.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="demo">
+    <h1>DataValue = {{ DataValue }}</h1>
+    <a-button @click="changeDataValue">Change DataValue</a-button>
+  </div>
+</template>
+
+<script lang="ts">
+import {defineComponent, ref, onBeforeMount} from "vue";
+
+export default defineComponent({
+  name: "Demo",
+  setup() {
+    const DataValue = ref("111");
+
+    const changeDataValue = () => {
+      DataValue.value = "222!";
+    };
+
+    // beforeMount hook
+    // 生命周期: onBeforeMount, onMounted, onBeforeUpdate, onUpdated, onBeforeUnmount, onUnmounted
+    onBeforeMount(() => {
+      console.log("DemoComponents beforeMount hook");
+    });
+
+    return {
+      DataValue,
+      changeDataValue,
+    };
+  },
+});
+</script>
+
+<style scoped>
+</style>
