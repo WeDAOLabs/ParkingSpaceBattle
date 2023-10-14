9b3ac8635830e04f822b654e8ea64bb52958fb1c
0xSkipper <0xSkipper@gmail.com>
2023-10-14 17:51:49 +0800

    header bar

--- client/website/src/components/Header.vue
+++ client/website/src/components/Header.vue
@@ -1,51 +1,67 @@
 <template>
-    <div>
-      <a-row>
-        <a-col :offset="1" :span="3">
-          <a-image :width="438" :src="require('../assets/logo.jpg')" :preview="false"/>
-        </a-col>
-        <a-col :offset="16" :span="2">
-            <a-button v-if="!isLogin" @click="connectWallet">Sign In</a-button>
-            <h3 v-else>address:{{ userAddress }}</h3>
-        </a-col>
-      </a-row>
-    </div>
+  <div class="header">
+    <a-row>
+      <a-col :offset="1" :span="3">
+        <a-image class="logo" :src="require('../assets/logo.jpg')" :preview="false"/>
+      </a-col>
+      <a-col :offset="16" :span="2">
+        <a-button v-if="!isLogin" @click="connectWallet">Sign In</a-button>
+        <h3 v-else>address:{{ userAddress }}</h3>
+      </a-col>
+    </a-row>
+  </div>
 </template>
-  
-  <script lang="ts">
-  import {defineComponent, ref, onBeforeMount} from "vue";
-  import {walletData} from '../data/WalletData';
-  
-  export default defineComponent({
-    name: "Header",
-  
-    components: {},
-    setup() {
-      const isLogin = ref(walletData.isAuth);
-      const userAddress = ref('0x12345');
-  
-      const connectWallet = async () => {
-        await walletData.signIn();
-        isLogin.value = walletData.isAuth;
-      };
 
-      onBeforeMount(()=>{
-    
+<script lang="ts">
+import {defineComponent, ref, onBeforeMount} from "vue";
+import {walletData} from '../data/WalletData';
+
+export default defineComponent({
+  name: "Header",
+
+  components: {},
+  setup() {
+    const isLogin = ref(walletData.isAuth);
+    const userAddress = ref('0x12345');
+
+    const connectWallet = async () => {
+      await walletData.signIn();
+      isLogin.value = walletData.isAuth;
+    };
+
+    onBeforeMount(() => {
+
+    });
+
+    return {
+      isLogin,
+      userAddress,
+      connectWallet
+    };
+  },
 });
-  
-      return {
-        isLogin,
-        userAddress,
-        connectWallet
-      };
-    },
-  });
 
-  onBeforeMount(()=>{
+onBeforeMount(() => {
 
-  });
-  </script>
-  
-  <style scoped>
-  
-  </style>
+});
+</script>
+
+<style scoped>
+
+.header {
+  position: fixed;
+  top: 0;
+  left: 0;
+  width: 100%;
+  background-color: #c9c9c9; /* 页眉的背景颜色 */
+  padding: 10px 0; /* 页眉的内边距 */
+  text-align: center;
+  z-index: 100; /* 控制页眉的层叠顺序 */
+  box-shadow: 0 2px 4px rgba(0, 0, 0, 0.5); /* 为页眉添加阴影效果 */
+}
+
+.logo {
+  width: 50%;
+  height: 50%;
+}
+</style>
