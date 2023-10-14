0b40804a18ba197f0ff0918d4caa4ab4f1997d1d
周正 <119305774+zhouzheng66@users.noreply.github.com>
2023-10-14 10:49:09 +0800

    2023/8/23fetch

---
+++ contracts/contracts/tokens/ParkREC721.sol
@@ -0,0 +1,60 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.20;
+
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Burnable.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+
+contract LOOTLOTPARK is ERC721, ERC721URIStorage, ERC721Burnable, Ownable {
+    address NFTminter;
+    uint256 private _nextTokenId;
+
+    constructor(address initialOwner)
+        ERC721("LOOTLOTPARK", "LLP")
+        Ownable(initialOwner)
+    {}
+
+    function _baseURI() internal pure override returns (string memory) {
+        return "https://lotloot.osairo.xyz/lotloot_parking_meta.json?tokenid=";
+    }
+
+    function safeMint(address to, string memory uri) public onlyOwner {
+        uint256 tokenId = _nextTokenId++;
+        _safeMint(to, tokenId);
+        _setTokenURI(tokenId, uri);
+    }
+     function setNFTminter(address _NFTminter) public onlyOwner {
+        NFTminter = _NFTminter;
+    }
+    function getNFTminter() public view returns (address) {
+        return NFTminter;
+    }
+    // the gammer mint
+    function NFTminterMint(address to, string memory uri) public{
+        require(msg.sender == NFTminter, "Only NFTminter can mint");
+        uint256 tokenId = _nextTokenId++;
+        _safeMint(to, tokenId);
+        _setTokenURI(tokenId, uri);
+    }
+
+    // The following functions are overrides required by Solidity.
+
+    function tokenURI(uint256 tokenId)
+        public
+        view
+        override(ERC721, ERC721URIStorage)
+        returns (string memory)
+    {
+        return super.tokenURI(tokenId);
+    }
+
+    function supportsInterface(bytes4 interfaceId)
+        public
+        view
+        override(ERC721, ERC721URIStorage)
+        returns (bool)
+    {
+        return super.supportsInterface(interfaceId);
+    }
+}
