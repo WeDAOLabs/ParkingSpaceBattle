4e821165636683f4fa322ab1c4aff1b0f72c948d
0xSkipper <0xSkipper@gmail.com>
2023-10-14 13:53:36 +0800

    车位 静态

--- client/website/src/components/Index.vue
+++ client/website/src/components/Index.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="index">
-    <a-row class="topBar">
+    <a-row class="top-bar">
       <a-col :offset="1" :span="3">
         <h1>LOGO</h1>
       </a-col>
@@ -18,24 +18,29 @@
         </a-col>
       </a-row>
     </div>
-    <div>
-      <a-row space-between>
-        <a-col :span="4">
-          <div>车位1</div>
-        </a-col>
-        <a-col :span="4">
-          <div>车位2</div>
-        </a-col>
-        <a-col :span="4">
-          <div>车位3</div>
-        </a-col>
-        <a-col :span="4">
-          <div>车位4</div>
-        </a-col>
-        <a-col :span="4">
-          <div>车位5</div>
-        </a-col>
+    <div v-else>
+      <a-divider/>
+      <a-row justify="space-around">
 
+        <a-col :span="4" v-for="(item, index) in userCarPortStateList" :key="index">
+          <a-row justify="center" align="middle" class="carport-0" v-if="item===0">
+            <a-button @click="funcFreeMint(index)">Free Mint</a-button>
+          </a-row>
+          <a-row justify="center" align="middle" class="carport-1" v-if="item===1">
+            <h2>空车位</h2>
+          </a-row>
+          <a-row justify="center" align="middle" class="carport-2" v-if="item===2">
+            <a-col :span="24">
+              <h3>当前车辆URL: xxx</h3>
+            </a-col>
+            <a-col :span="24">
+              <a-button @click="funcSticker(index)">贴条</a-button>
+            </a-col>
+            <a-col :span="24">
+              <p>预计收益: 123 LLT</p>
+            </a-col>
+          </a-row>
+        </a-col>
       </a-row>
     </div>
 
@@ -44,13 +49,22 @@
 </template>
 
 <script lang="ts">
-import {defineComponent, ref, onBeforeMount} from "vue";
+import {defineComponent, ref, onBeforeMount, computed} from "vue";
 
 export default defineComponent({
   name: "Index",
   setup() {
     const isLogin = ref(false);
     const userAddress = ref('0x12345');
+    const userCarPortStateList = ref([1, 2, 1, 0, 0]);// 0表示未mint 1表示车位空 2表示占用
+
+    const funcFreeMint = (index) => {
+      userCarPortStateList.value[index] = 1;
+    };
+
+    const funcSticker = (index) => {
+      userCarPortStateList.value[index] = 1;
+    };
 
     const funcLogin = () => {
       isLogin.value = true;
@@ -59,14 +73,42 @@ export default defineComponent({
     return {
       isLogin,
       userAddress,
-      funcLogin
+      userCarPortStateList,
+      funcLogin,
+      funcFreeMint,
+      funcSticker
     };
   },
 });
 </script>
 
 <style scoped>
-.topBar {
+.top-bar {
   margin-top: -40px;
 }
+
+.carport-0 {
+  border: 1px solid #8ce6b0;
+  border-radius: 0.5rem;
+  height: 15rem;
+  box-sizing: content-box;
+  background-color: #edfff3;
+}
+
+.carport-1 {
+  border: 1px solid #abdcff;
+  border-radius: 0.5rem;
+  height: 15rem;
+  box-sizing: content-box;
+  background-color: #f0faff;
+}
+
+.carport-2 {
+  border: 1px solid #ffd77a;
+  border-radius: 0.5rem;
+  height: 15rem;
+  box-sizing: content-box;
+  background-color: #fff9e6;
+}
+
 </style>
--- client/website/src/main.ts
+++ client/website/src/main.ts
@@ -2,13 +2,13 @@ import { createApp } from "vue";
 import App from "./App.vue";
 // @ts-ignore
 import Antd from "ant-design-vue";
-import { ethers } from "ethers";
+// import { ethers } from "ethers";
 import "ant-design-vue/dist/reset.css";
 
 const app = createApp(App);
 
 app.use(Antd);
 
-app.config.globalProperties.$ethers = ethers;
+// app.config.globalProperties.$ethers = ethers;
 
 app.mount("#app");
