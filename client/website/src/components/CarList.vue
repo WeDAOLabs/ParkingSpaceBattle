083f3d85eb1fb03dec5d558c63b6044949beb307
小e无双 <405284695@qq.com>
2023-10-16 03:12:32 +0800

    modify

--- client/website/src/components/CarList.vue
+++ client/website/src/components/CarList.vue
@@ -8,26 +8,41 @@
       </a-col> -->
       <a-col
         :span="4"
-        v-for="(item, index) in userCarStateList"
-        :key="index"
+        v-for="item in userCarStateList"
+        :key="item.tokenId"
         class="carMargin"
       >
-        <a-row justify="center" align="middle" class="car-0" v-if="item === 0">
+        <a-row
+          justify="center"
+          align="middle"
+          class="car-0"
+          v-if="item.status === 0"
+        >
           <a-button @click="funcFreeMintCar()">Buy Car</a-button>
         </a-row>
-        <a-row justify="center" align="middle" class="car-1" v-if="item === 1">
+        <a-row
+          justify="center"
+          align="middle"
+          class="car-1"
+          v-if="item.status === 1"
+        >
           <a-col :span="24">
             <h2>车辆URL: XXX</h2>
-            <h3>车辆ID: XXX</h3>
+            <h3>车辆ID: {{ item.tokenId }}</h3>
           </a-col>
-          <a-col :span="12">
+          <a-col :span="12" v-if="!isHome">
             <a-button>去停车</a-button>
           </a-col>
-          <a-col :span="12">
+          <a-col :span="12" v-if="isHome">
             <a-button>去升级</a-button>
           </a-col>
         </a-row>
-        <a-row justify="center" align="middle" class="car-2" v-if="item === 2">
+        <a-row
+          justify="center"
+          align="middle"
+          class="car-2"
+          v-if="item.status === 2"
+        >
           <a-col :span="24">
             <h2>车辆URL: XXX</h2>
           </a-col>
@@ -83,21 +98,25 @@ export default defineComponent({
     const refreshCar = async () => {
       Loading.close();
       const player = await playerData.getPlayerData(homeData.currentPlyer);
-      if (player) {
-        const status = player.hasCars
-          ? player.cars.map((car) => car.status)
-          : [];
-        if (homeData.isInHome) {
-          status.push(CarStatus.UN_MINT);
-        }
-        //@ts-ignore
-        userCarStateList.value = status;
-      } else {
-        userCarStateList.value = [];
+      isHome.value = homeData.isInHome;
+      let cars = player
+        ? player.cars.map((car) => {
+            return { tokenId: car.tokenId, status: car.status };
+          })
+        : [];
+      if (isHome.value) {
+        cars.push({
+          tokenId: 0,
+          status: CarStatus.UN_MINT,
+        });
       }
+
+      //@ts-ignore
+      userCarStateList.value = cars;
     };
 
     const userCarStateList = ref([]);
+    const isHome = ref(true);
 
     const funcFreeMintCar = async () => {
       Loading.open();
@@ -116,6 +135,7 @@ export default defineComponent({
       userCarStateList,
       funcFreeMintCar,
       funcBuyCar,
+      isHome,
     };
   },
 });
