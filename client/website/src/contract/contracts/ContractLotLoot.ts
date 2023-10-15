25e0253a326127f9f7e4a11cfb1371423668eeb3
小e无双 <405284695@qq.com>
2023-10-15 20:43:48 +0800

    modify

--- client/website/src/contract/contracts/ContractLotLoot.ts
+++ client/website/src/contract/contracts/ContractLotLoot.ts
@@ -1,11 +1,11 @@
 import { ContractBase } from "./ContractBase";
-import ContractLotLootAI from "../abi/contracts/systems/core/LotLoot.sol/LotLoot.json";
+import ContractLotLootABI from "../abi/contracts/systems/core/LotLoot.sol/LotLoot.json";
 import { contractData } from "../../data/ContractData";
 
 export class ContractLotLoot extends ContractBase {
   static create(): any {
     const address = contractData.contractAddress.LotLoot;
-    const contract = new ContractLotLoot(ContractLotLootAI, address, "", "");
+    const contract = new ContractLotLoot(ContractLotLootABI, address, "", "");
     return contract.createContract();
   }
 
