7975e324272a227f03ea07c1d4660577be06ee5a
小e无双 <405284695@qq.com>
2023-10-15 16:17:48 +0800

    modify index

--- client/website/src/components/Index.vue
+++ client/website/src/components/Index.vue
@@ -1,113 +1,53 @@
 <template>
   <div class="index">
-    <Header class="margin-top"/>
+    <Header class="margin-top" />
     <div v-if="!isLogin">
-      <a-divider/>
+      <a-divider />
       <a-row>
         <a-col :offset="5" :span="10">
-          <WelcomePage/>
-          <h1>展示游戏大图介绍内容</h1>
+          <WelcomePage />
         </a-col>
       </a-row>
     </div>
     <div v-else>
-      <a-divider/>
-      <SearchBar/>
-      <a-divider/>
-      <ParkingList/>
-      <a-divider/>
-      <CarList/>
+      <a-divider />
+      <SearchBar />
+      <a-divider />
+      <ParkingList />
+      <a-divider />
+      <CarList />
     </div>
   </div>
 </template>
 
 <script lang="ts">
-import {defineComponent, ref, onBeforeMount, onUnmounted} from "vue";
+import { defineComponent, ref, onBeforeMount, onUnmounted } from "vue";
 import Header from "./Header.vue";
 import ParkingList from "./ParkingList.vue";
 import SearchBar from "./SearchBar.vue";
 import CarList from "./CarList.vue";
 import WelcomePage from "./WelcomePage.vue";
 
-
-import {walletData} from "../data/WalletData";
-import {EventBus} from "../plugins/EventBus";
-import {GameEventSample} from "../events/GameEventSample";
-import {GameEventWalletDisconnect} from "../events/GameEventWalletDisconnect";
-import {GameEventWalletConnected} from "../events/GameEventWalletConnected";
-import {GameEventWalletAccountChanged} from "../events/GameEventWalletAccountChanged";
+import { walletData } from "../data/WalletData";
+import { EventBus } from "../plugins/EventBus";
+import { GameEventSample } from "../events/GameEventSample";
+import { GameEventWalletDisconnect } from "../events/GameEventWalletDisconnect";
+import { GameEventWalletConnected } from "../events/GameEventWalletConnected";
+import { GameEventWalletAccountChanged } from "../events/GameEventWalletAccountChanged";
 
 export default defineComponent({
   name: "Index",
 
-  components: {WelcomePage, Header, ParkingList, CarList, SearchBar},
+  components: { WelcomePage, Header, ParkingList, CarList, SearchBar },
   setup() {
     const isLogin = ref(walletData.isAuth);
-
-
-
-
-    // const showSkin = ref(false);
-
-    // async function someAsyncOperation() {
-    //   return new Promise((resolve, reject) => {
-    //     // 模拟异步操作，比如网络请求
-    //     setTimeout(() => {
-    //       // 模拟操作成功，将结果返回
-    //       resolve('操作成功！');
-    //
-    //       // 如果出现错误，可以使用 reject
-    //       // reject('操作失败！');
-    //     }, 2000);
-    //   });
-    // }
-    //
-    // async function funcLogin() {
-    //   isLogin.value = true;
-    //
-    //   try {
-    //     // 异步操作，比如网络请求、文件读取等
-    //     showSkin.value = true;
-    //     const result = await someAsyncOperation();
-    //     console.log('操作结果:', result);
-    //     showSkin.value = false;
-    //   } catch (error) {
-    //     console.error('发生错误:', error);
-    //     showSkin.value = false;
-    //   }
-    // }
-
-    // const funcLogin = async () => {
-    //   try {
-    //     showSkin.value = true;
-    //     const result = await someAsyncOperation();
-    //     console.log('操作结果:', result);
-    //     showSkin.value = false;
-    //     isLogin.value = true;
-    //   } catch (error) {
-    //     console.error('发生错误:', error);
-    //   }
-    // };
-    //
-    // const someAsyncOperation = async () => {
-    //   return new Promise((resolve, reject) => {
-    //     // 模拟异步操作，比如网络请求
-    //     setTimeout(() => {
-    //       // 模拟操作成功，将结果返回
-    //       resolve('操作成功！');
-    //
-    //       // 如果出现错误，可以使用 reject
-    //       // reject('操作失败！');
-    //     }, 2000);
-    //   });
-    // };
 
     onBeforeMount(() => {
       EventBus.instance.on(GameEventWalletConnected.event, onSignIn);
       EventBus.instance.on(GameEventWalletDisconnect.eventAsync, onSignOut);
       EventBus.instance.on(
-          GameEventWalletAccountChanged.eventAsync,
-          onAccountChanged
+        GameEventWalletAccountChanged.eventAsync,
+        onAccountChanged
       );
     });
 
@@ -115,8 +55,8 @@ export default defineComponent({
       EventBus.instance.off(GameEventWalletConnected.event, onSignIn);
       EventBus.instance.off(GameEventWalletDisconnect.eventAsync, onSignOut);
       EventBus.instance.off(
-          GameEventWalletAccountChanged.eventAsync,
-          onAccountChanged
+        GameEventWalletAccountChanged.eventAsync,
+        onAccountChanged
       );
     });
 
@@ -128,8 +68,7 @@ export default defineComponent({
       isLogin.value = false;
     };
 
-    const onAccountChanged = () => {
-    };
+    const onAccountChanged = () => {};
 
     return {
       isLogin,
