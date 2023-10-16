de20d59036d5454af847b000980171b263211b12
小e无双 <405284695@qq.com>
2023-10-16 11:21:40 +0800

    add cache

--- client/website/src/contract/contracts/ContractRegistry6551.ts
+++ client/website/src/contract/contracts/ContractRegistry6551.ts
@@ -2,6 +2,7 @@ import { ContractBase } from "./ContractBase";
 import ContractERC6551RegistryAI from "../abi/contracts/systems/ERC6551/ERC6551Registry.sol/ERC6551Registry.json";
 import { contractData } from "../../data/ContractData";
 import { walletData } from "@/data/WalletData";
+import { IndexDB } from "@/plugins/indexDB";
 
 export class ContractERC6551Registry extends ContractBase {
   static create(): any {
@@ -16,6 +17,12 @@ export class ContractERC6551Registry extends ContractBase {
   }
 
   async account(tokenId: number, nftContractAddress: string) {
+    const cacheKey = `6551_account_${tokenId}_${nftContractAddress}_${walletData.chainId}`;
+    const cache = await IndexDB.instance.getItem(cacheKey);
+    if (cache) {
+      return cache;
+    }
+
     const account = await this.contract.account(
       contractData.contractAddress.Account6551,
       walletData.chainId,
@@ -24,6 +31,8 @@ export class ContractERC6551Registry extends ContractBase {
       tokenId,
       []
     );
+
+    await IndexDB.instance.addItem(cacheKey, account);
 
     return account;
   }
