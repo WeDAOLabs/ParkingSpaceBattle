c3542c1546c95702af1c2a1b098ca6818b5c9215
enixlee <405284695@qq.com>
2023-10-20 16:27:59 +0800

    modify

--- contracts/scripts/systems/core/CarStore.deploy.ts
+++ contracts/scripts/systems/core/CarStore.deploy.ts
@@ -8,10 +8,17 @@ import { EonDeploy } from "../../deploy/eon-deploy.class";
 
 async function main() {
   const deployer = new EonDeploy();
+  // mumbai
+  // const contract = await deployer.deployUpgradeWithData("CarStore", [
+  //   "0x4DDbF5Ddf8819dE761fC708F8A522D341E03cBE5",
+  //   "0x38cf1C772EC1121b5a9cc1A4CCE07B25c71305eE",
+  //   "0x6cf706A92a234652f36995e5004c4Cb72483E529",
+  // ]);
+  // scroll
   const contract = await deployer.deployUpgradeWithData("CarStore", [
-    "0x4DDbF5Ddf8819dE761fC708F8A522D341E03cBE5",
-    "0x38cf1C772EC1121b5a9cc1A4CCE07B25c71305eE",
-    "0x6cf706A92a234652f36995e5004c4Cb72483E529",
+    "0x51e49799490A4469fb73edFC09822b3b566cE445",
+    "0x6550755AEE41CC87E72A849Fdf9022aa74DEC1F4",
+    "0xf412719c2dDe7e83B2Ff3bDdBE92C48525C4DE18",
   ]);
   console.log("deployed to:", contract.address);
 }
